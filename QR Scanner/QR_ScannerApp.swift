fbe6d244fc690cb9331c029e21fd519af90b8699
Brent Amersbach <triumshockwave@gmail.com>
2025-02-14 22:18:17 -0500

    Initial Commit

---
+++ QR Scanner/QR_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QR_ScannerApp.swift
+//  QR Scanner
+//
+//  Created by Brent Amersbach on 2/14/25.
+//
+
+import SwiftUI
+
+@main
+struct QR_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
