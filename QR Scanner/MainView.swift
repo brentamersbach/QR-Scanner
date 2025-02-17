65f1181930f367958229e4995df95bc342c8222c
Brent Amersbach <triumshockwave@gmail.com>
2025-02-17 18:49:00 -0500

    Copy, Clear and state restoration all working

--- QR Scanner/MainView.swift
+++ QR Scanner/MainView.swift
@@ -9,9 +9,20 @@ import SwiftUI
 import CodeScanner
 
 struct MainView: View {
+    var isPreview: Bool {
+        return ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1"
+    }
     
     @State private var isShowingScanner = false
-    @State private var resultString: String = ""
+    @State private var isShowingCopyConfirmation: Bool = false
+    #if targetEnvironment(simulator)
+        @State private var resultString: String = "Hello, World! I'm a bunch of data from a QR code!"
+    #else
+        @SceneStorage("resultString")
+        private var resultString: String = ""
+    #endif
+    let overlayColor = Color(UIColor.secondarySystemBackground)
+    let clipboard = UIPasteboard.general
     
     func handleScan(result: Result<ScanResult, ScanError>) {
        isShowingScanner = false
@@ -24,24 +35,66 @@ struct MainView: View {
             print("Scanning failed: \(error.localizedDescription)")
         }
     }
+    
+    func copyToClipboard(_ text: String) {
+        clipboard.string = resultString
+        isShowingCopyConfirmation.toggle()
+        withAnimation(.easeOut(duration: 1.5)) {
+            isShowingCopyConfirmation.toggle()
+        }
+    }
 
     var body: some View {
-        VStack {
-            Button(action: {
-                isShowingScanner = true
-            }) {
-                Text("Scan QR Code")
+        ZStack {
+            if isPreview || isShowingCopyConfirmation {
+                Text("Text copied to clipboard")
                     .font(.title)
+                    .padding()
+                    .background(overlayColor)
+                    .frame(alignment: .center)
+                    .cornerRadius(20)
             }
-            .padding(16)
-            Text("Code data:")
-                .font(.headline)
-            ScrollView {
-                Text(resultString)
+            VStack {
+                Text("Code data:")
+                    .font(.headline)
+                ScrollView {
+                    if resultString.lengthOfBytes(using: .utf8) > 0 {
+                        Text(resultString)
+                            .monospaced()
+                            .padding(.leading, 16)
+                            .padding(.trailing, 16)
+                    } else {
+                        Text("Tap Scan QR Code to begin")
+                            .opacity(0.7)
+                            .padding(.leading, 16)
+                            .padding(.trailing, 16)
+                    }
+                }
+                Button(action: {
+                    isShowingScanner = true
+                }) {
+                    Text("Scan QR Code")
+                        .font(.title)
+                }
+                .padding(16)
+                if resultString.lengthOfBytes(using: .utf8) > 0 {
+                    HStack(spacing: 16) {
+                        Button("Copy") {
+                            copyToClipboard(resultString)
+                        }
+                        .font(.title2)
+                        Spacer()
+                            .frame(maxWidth: 20)
+                        Button("Clear", role: .destructive) {
+                            resultString = ""
+                        }
+                        .font(.title2)
+                    }
+                }
             }
-        }
-        .sheet(isPresented: $isShowingScanner) {
-            CodeScannerView(codeTypes: [.qr], simulatedData: "Berry cat is the cattest cat", completion: handleScan)
+            .sheet(isPresented: $isShowingScanner) {
+                CodeScannerView(codeTypes: [.qr, .ean8, .ean13, .gs1DataBar, .gs1DataBarLimited, .gs1DataBarExpanded], simulatedData: "Berry cat is the cattest cat", completion: handleScan)
+            }
         }
     }
 }
